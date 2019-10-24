c684862b1fb8635a4c012d5dc4921dd129bb3c30
Dhanush Adithya <s.dhanushadithya@gmail.com>
2019-10-24 15:38:29 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,4 +5,4 @@ client.once('ready', () => {
     console.log(`Ready! ${client.user.tag}`);
 });
 
-client.login('NjM2NTQ1NzYyMjQyNTkyNzY5.XbF1yw.cqoTelUKc9fZVBZnnCr3PTOUesE');
+client.login('NjM2NTQ1NzYyMjQyNTkyNzY5.XbF35A.ihH5XBopBK3Vk530x_BCQ3mx5_I');
