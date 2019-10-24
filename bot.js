837dbc065315896efb48267df2378f453a265640
Dhanush Adithya <s.dhanushadithya@gmail.com>
2019-10-24 15:56:09 +0530

    Updated everything

--- bot.js
+++ bot.js
@@ -5,4 +5,4 @@ client.on('ready', () => {
     console.log(`Ready! ${client.user.tag}`);
 });
 
-client.login('NjM2NTQ1NzYyMjQyNTkyNzY5.XbF5lg.JvywihcgXnb22k8aOmypel__vYU');
+client.login(process.env.BOT_TOKEN);
