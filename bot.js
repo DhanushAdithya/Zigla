f0764056142e264b17ed7b56cb81206389c4f7ae
Dhanush Adithya <s.dhanushadithya@gmail.com>
2019-10-24 15:08:35 +0530

    Test

--- bot.js
+++ bot.js
@@ -5,4 +5,4 @@ client.on('ready', () => {
     console.log(`Connected as ${client.user.tag}`)
 })
 
-client.login('NjM2NTQ1NzYyMjQyNTkyNzY5.XbFhuA.ZlTYU6kTN8LGp5dotcoXBrAxV7Q')
+client.login('NjM2NTQ1NzYyMjQyNTkyNzY5.XbFk0w.zAQLiNIQB8HI9HtxAfK3QQYMlB0')
