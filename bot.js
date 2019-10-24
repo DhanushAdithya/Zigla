b4ceb66a5b52b7016569466ed0c4eeb0610f4c7c
Dhanush Adithya <s.dhanushadithya@gmail.com>
2019-10-24 14:50:04 +0530

    Initial commit

---
+++ bot.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js')
+const client = new Discord.client()
+
+client.on('ready', () => {
+    console.log(`Connected as ${client.user.tag}`)
+})
+
+client.login('NjM2NTQ1NzYyMjQyNTkyNzY5.XbFhuA.ZlTYU6kTN8LGp5dotcoXBrAxV7Q')
