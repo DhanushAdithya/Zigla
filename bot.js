2db5fdc707b6bbc00f5fdfd801d4068bf8aa70db
Dhanush Adithya <s.dhanushadithya@gmail.com>
2019-10-24 15:11:25 +0530

    Updated bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,14 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log(`Connected as ${client.user.tag}`)
-})
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('Pong!');
+    }
+});
 
 client.login('NjM2NTQ1NzYyMjQyNTkyNzY5.XbFk0w.zAQLiNIQB8HI9HtxAfK3QQYMlB0')
