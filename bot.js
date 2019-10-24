e77896c1bc71e1b102461c23bdeef790c40f9b62
Dhanush Adithya <s.dhanushadithya@gmail.com>
2019-10-24 14:59:37 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const client = new Discord.client()
 
 client.on('ready', () => {
-    console.log(`${client.user.tag} connected`)
+    console.log(`Connected as ${client.user.tag}`)
 })
 
 client.login('NjM2NTQ1NzYyMjQyNTkyNzY5.XbFhuA.ZlTYU6kTN8LGp5dotcoXBrAxV7Q')
