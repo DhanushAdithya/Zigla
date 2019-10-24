e4c753d44b6c996cfaecf2a7e87b220403c378f9
Dhanush Adithya <s.dhanushadithya@gmail.com>
2019-10-24 19:19:03 +0530

    Update on bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+console.log(`${client.user.email}`)
+
 client.on('ready', () => {
     console.log(`Ready! ${client.user.tag}`);
 });
