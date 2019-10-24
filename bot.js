e6e9927336e90a7e36f25f09065a321ba675c171
Dhanush Adithya <s.dhanushadithya@gmail.com>
2019-10-24 15:30:07 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+client.once('ready', () => {
+    console.log(`Ready! ${client.user.tag}`);
 });
 
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('Pong!');
-    }
-});
-
-client.login('NjM2NTQ1NzYyMjQyNTkyNzY5.XbFy1g.6EfROaWGLgKj52phDD3Lu3D5rko')
+client.login('NjM2NTQ1NzYyMjQyNTkyNzY5.XbF1yw.cqoTelUKc9fZVBZnnCr3PTOUesE');
