f38485cc1a0140c5897658b6cba69971082a3dec
Dhanush Adithya <s.dhanushadithya@gmail.com>
2019-12-12 18:43:28 +0530

    Formatting in the cmd

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,4 @@
-const helps = '**Zigla Bot Help**\n**Fun Commands**\n`z#pup` - Get a puppy\n`z#avatar` - Get your Discord avatar with its link\n`z#ping` - Pong!\n';
+const helps = '**Zigla Bot Help**\n**Fun Commands**\n\n\t`z#pup` - Get a puppy\n\t`z#avatar` - Get your Discord avatar with its link\n\t`z#ping` - Pong!\n';
 exports.run = (client, message, args) => {
     message.channel.send(helps).catch(err => console.error(err))
 }
