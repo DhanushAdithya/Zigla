d8d46164d155324e6316f2ead7bacfce9a6272ac
Dhanush Adithya <s.dhanushadithya@gmail.com>
2019-12-12 16:36:17 +0530

    Updated the js files

--- bot.js
+++ bot.js
@@ -16,58 +16,64 @@ client.on('ready', () => {
 
 client.on('message', message => {
 
+    if (message.author.bot) return;
+    if (message.content.indexOf(prefix) !== 0) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
     const cmd = client.commands.get(command);
     if (!cmd) return;
 
     cmd.run(client, message, args);
 
-    axios.get('https://dog.ceo/api/breeds/image/random')
-        .then(res => res.data)
-        .then(data => {
-            l = (data.message).split('/')
-            l = l[4]
-            if (l.includes('-')) {
-                l = l.split('-').map(element => element.charAt(0).toUpperCase() + element.slice(1)).join(' ')
-                embed
-                    .setColor(0x00AE86)
-                    .setImage(data.message)
-                    .setDescription(`**Breed**: ${l}`)
-            } else {
-                l = l.charAt(0).toUpperCase() + l.slice(1)
-                embed
-                    .setColor(0x00AE86)
-                    .setImage(data.message)
-                    .setDescription(`**Breed**: ${l}`)
-            }
-        })
-        .catch(err => console.warn(err))
+    // axios.get('https://dog.ceo/api/breeds/image/random')
+    //     .then(res => res.data)
+    //     .then(data => {
+    //         l = (data.message).split('/')
+    //         l = l[4]
+    //         if (l.includes('-')) {
+    //             l = l.split('-').map(element => element.charAt(0).toUpperCase() + element.slice(1)).join(' ')
+    //             embed
+    //                 .setColor(0x00AE86)
+    //                 .setImage(data.message)
+    //                 .setDescription(`**Breed**: ${l}`)
+    //         } else {
+    //             l = l.charAt(0).toUpperCase() + l.slice(1)
+    //             embed
+    //                 .setColor(0x00AE86)
+    //                 .setImage(data.message)
+    //                 .setDescription(`**Breed**: ${l}`)
+    //         }
+    //     })
+    //     .catch(err => console.warn(err))
 
-    let msg = message.content;
-    if (msg === `${prefix}dog`) {
-        message.channel.send(embed);
-    }
-});
-
-client.on('message', msg => {
-    // if ((msg.content).toLowerCase() === 'ping') {
-    //     msg.reply('Pong!');
+    // let msg = message.content;
+    // if (msg === `${prefix}dog`) {
+    //     message.channel.send(embed);
     // }
-
-    if (msg.content === `${prefix}hi`) {
-        msg.reply('Hello!');
-    }
-
-    if (msg.content === 'avatar') {
-        msg.reply(msg.author.avatarURL);
-    }
 });
 
-fs.readdir('./cmds/', async (err, files) => {
+// client.on('message', msg => {
+//     if ((msg.content).toLowerCase() === 'ping') {
+//         msg.reply('Pong!');
+//     }
+
+//     if (msg.content === `${prefix}hi`) {
+//         msg.reply('Hello!');
+//     }
+
+//     if (msg.content === 'avatar') {
+//         msg.reply(msg.author.avatarURL);
+//     }
+// });
+
+fs.readdir('./commands/', async (err, files) => {
     if (err) return console.error(err);
 
     files.forEach(file => {
         if (!file.endsWith('.js')) return;
-        let props = reuire(`./cmds/${file}`);
+        let props = reuire(`./commands/${file}`);
         let cmdName = file.split('.')[0];
         console.log(`Loaded cmd : ${cmdName}`);
         client.commands.set(cmdName, props);
---
+++ commands/ping.js
@@ -0,0 +1,7 @@
+exports.run = (client, message, args) => {
+    message.channel.send('Pong!').catch(err => console.error(err))
+};
+
+exports.help = {
+    name: 'ping'
+}
