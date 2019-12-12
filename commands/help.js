e10d605cd83a6a1d8990d421fc1b69d1e325fd2d
Dhanush Adithya <s.dhanushadithya@gmail.com>
2019-12-12 18:15:47 +0530

    Update

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 const helps = '**Zigla Bot Help**\n**Fun Commands**\n**`z#pup`** - Get a puppy\n**`z#avatar`** - Get your Discord avatar with its link\n**`z#ping`** - Pong!\n';
 exports.run = (client, message, args) => {
-    message.channel.send(helps).setTimestamp().catch(err => console.error(err))
+    message.channel.send(helps).catch(err => console.error(err))
 }
 
 exports.help = {
