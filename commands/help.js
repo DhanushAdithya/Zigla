69461042f6dd42532f62e67de7247ad5e45c125a
Dhanush Adithya <s.dhanushadithya@gmail.com>
2019-12-12 18:04:06 +0530

    Updated the help cmd

--- commands/help.js
+++ commands/help.js
@@ -7,6 +7,8 @@ exports.run = (client, message, args) => {
         .setAuthor(`@${message.author.tag}`)
         .setThumbnail(message.author.avatarURL)
         .setDescription('\n**Fun Commands**\n**`z#pup`** - Get a puppy\n**`z#avatar`** - Get your Discord avatar with its link\n**`z#ping`** - Pong!\n')
+        .setTitle('**Zigla Bot Help**')
+        .setTimestamp()
     message.channel.send(embed).catch(err => console.error(err))
 }
 
