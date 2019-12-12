996636ded1660743bf711e3fc0489abfbb1002b1
Dhanush Adithya <s.dhanushadithya@gmail.com>
2019-12-12 17:03:09 +0530

    Pup cmd 🐕🐕‍🦺🐩🐶 hey 🎉🎉

--- bot.js
+++ bot.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const axios = require('axios');
 const fs = require('fs');
 const Enmap = require('enmap');
 const client = new Discord.Client();
@@ -26,32 +25,6 @@ client.on('message', message => {
     if (!cmd) return;
 
     cmd.run(client, message, args);
-
-    // axios.get('https://dog.ceo/api/breeds/image/random')
-    //     .then(res => res.data)
-    //     .then(data => {
-    //         l = (data.message).split('/')
-    //         l = l[4]
-    //         if (l.includes('-')) {
-    //             l = l.split('-').map(element => element.charAt(0).toUpperCase() + element.slice(1)).join(' ')
-    //             embed
-    //                 .setColor(0x00AE86)
-    //                 .setImage(data.message)
-    //                 .setDescription(`**Breed**: ${l}`)
-    //         } else {
-    //             l = l.charAt(0).toUpperCase() + l.slice(1)
-    //             embed
-    //                 .setColor(0x00AE86)
-    //                 .setImage(data.message)
-    //                 .setDescription(`**Breed**: ${l}`)
-    //         }
-    //     })
-    //     .catch(err => console.warn(err))
-
-    // let msg = message.content;
-    // if (msg === `${prefix}dog`) {
-    //     message.channel.send(embed);
-    // }
 });
 
 fs.readdir('./commands/', async (err, files) => {
---
+++ commands/pup.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const axios = require('axios');
+
+const embed = new Discord.RichEmbed();
+
+axios.get('https://dog.ceo/api/breeds/image/random')
+    .then(res => res.data)
+    .then(data => {
+        l = (data.message).split('/')
+        l = l[4]
+        if (l.includes('-')) {
+            l = l.split('-').map(element => element.charAt(0).toUpperCase() + element.slice(1)).join(' ')
+            embed
+                .setColor(0x00AE86)
+                .setImage(data.message)
+                .setDescription(`**Breed**: ${l}`)
+        } else {
+            l = l.charAt(0).toUpperCase() + l.slice(1)
+            embed
+                .setColor(0x00AE86)
+                .setImage(data.message)
+                .setDescription(`**Breed**: ${l}`)
+        }
+    })
+    .catch(err => console.warn(err))
+
+exports.run = (client, message, args) => {
+    message.channel.send(embed).catch(err => console.error(err))
+};
+
+exports.help = {
+    name: 'pup'
+}
