e2f25307a82b4e39f4217e71027907a1900a1b05
Dhanush Adithya <s.dhanushadithya@gmail.com>
2019-12-12 17:45:02 +0530

    Updated

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ const embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
     embed
         .setColor(0xFFFFFF)
-        .setAuthor(message.member.displayName)
+        .setAuthor(message.author.username)
         .setThumbnail(message.author.avatarURL)
         .setDescription('\n**Fun Commands**\n`z#pup` - Get a puppy\n`z#avatar` - Get your Discord avatar with its link\n`z#ping` - Pong!\n')
     message.channel.send(embed).catch(err => console.error(err))
