455b0f11ff905adf1eac450c59d5cbbc40dba7ff
Dhanush Adithya <s.dhanushadithya@gmail.com>
2019-12-14 18:10:05 +0530

    Meowww cmd 🐈 🐱😸😹😺😻😼 😽😾😿🙀🐱‍👤🐱‍🏍 🐱‍💻🐱‍🐉🐱‍👓🐱‍🚀📇

---
+++ commands/meow.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const axios = require('axios');
+
+const embed = new Discord.RichEmbed();
+
+exports.run = (client, message, args) => {
+
+    axios.get('https://aws.random.cat/meow')
+        .then(res => res.data)
+        .then(data => embed.setColor(0x00AE86).setImage(data.file))
+        .catch(err => console.warn(err))
+
+    message.channel.send(embed).catch(err => console.error(err))
+};
+
+exports.help = {
+    name: 'meow'
+}
