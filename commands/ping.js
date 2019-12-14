81ff4111612e319881ee793437245b0a4450c482
Dhanush Adithya <s.dhanushadithya@gmail.com>
2019-12-14 18:45:45 +0530

    Updated ping.js again

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,8 @@
 exports.run = (client, message, args) => {
     const msg = await message.channel.send('🏓 Pinging ...')
-    msg
-        .edit(`🏓 Pong ! 
+    msg.edit(`🏓 Pong ! 
         Lag rate      : ${Math.floor(msg.createdAt - message.createdAt)}ms
         API lag rate  : ${client.ping}ms`)
-        .catch(err => console.error(err))
 };
 
 exports.help = {
