5ef6320460529c3a6cb8e2773151bf87a65851c1
Dhanush Adithya <s.dhanushadithya@gmail.com>
2019-12-14 15:33:29 +0530

    Added events and some codes in it 🙂

--- bot.js
+++ bot.js
@@ -3,27 +3,20 @@ const fs = require('fs');
 const Enmap = require('enmap');
 const client = new Discord.Client();
 
-const prefix = 'z#';
-
 client.commands = new Enmap();
 
-client.on('ready', () => {
-    console.log(`Ready! ${client.user.tag} !`);
-});
+fs.readdir('./events/', (err, files) => {
+    if (err) return console.error(err);
 
-client.on('message', message => {
+    files.forEach(file => {
+        if (!file.endsWith('.js')) return;
+        let evt = require(`./events/${file}`);
+        let evtName = file.split('.')[0];
+        console.log(`Loaded evt : ${evtName}`);
+        client.on(evtName, evt.bind(null, client));
 
-    if (message.author.bot) return;
-    if (message.content.indexOf(prefix) !== 0) return;
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-
-    const cmd = client.commands.get(command);
-    if (!cmd) return;
-
-    cmd.run(client, message, args);
-});
+    });
+})
 
 fs.readdir('./commands/', async (err, files) => {
     if (err) return console.error(err);
