646bc6bacbe4e33fad7cf69f13b82d7a1e92cf1a
Dhanush Adithya <s.dhanushadithya@gmail.com>
2019-12-14 18:30:59 +0530

    Updated

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,7 +4,7 @@ const embed = new Discord.RichEmbed();
 
 exports.run = (client, message, args) => {
     embed.setColor(0x00AE86).setImage(message.author.avatarURL)
-    message.reply(embed).catch(err => console.error(err))
+    message.channel.send(embed).catch(err => console.error(err))
 };
 
 exports.help = {
