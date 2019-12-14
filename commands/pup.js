d4697e7b92d3155557221628f748180f02c8bf68
Dhanush Adithya <s.dhanushadithya@gmail.com>
2019-12-14 17:31:17 +0530

    Updated pup.js

--- commands/pup.js
+++ commands/pup.js
@@ -4,27 +4,28 @@ const axios = require('axios');
 const embed = new Discord.RichEmbed();
 
 exports.run = (client, message, args) => {
+
     axios.get('https://dog.ceo/api/breeds/image/random')
-    .then(res => res.data)
-    .then(data => {
-        l = (data.message).split('/') 
-        l = l[4]
-        if (l.includes('-')) {
-            l = l.split('-').map(element => element.charAt(0).toUpperCase() + element.slice(1)).join(' ')
-            embed
-                .setColor(0x00AE86) 
-                .setImage(data.message)
-                .setDescription(`**Breed**: ${l}`)
-        } else {
-            l = l.charAt(0).toUpperCase() + l.slice(1)
-            embed
-                .setColor(0x00AE86)
-                .setImage(data.message)
-                .setDescription(`**Breed**: ${l}`)
-        }
-    })
-    .catch(err => console.warn(err))
-    
+        .then(res => res.data)
+        .then(data => {
+            l = (data.message).split('/')
+            l = l[4]
+            if (l.includes('-')) {
+                l = l.split('-').map(element => element.charAt(0).toUpperCase() + element.slice(1)).join(' ')
+                embed
+                    .setColor(0x00AE86)
+                    .setImage(data.message)
+                    .setDescription(`**Breed**: ${l}`)
+            } else {
+                l = l.charAt(0).toUpperCase() + l.slice(1)
+                embed
+                    .setColor(0x00AE86)
+                    .setImage(data.message)
+                    .setDescription(`**Breed**: ${l}`)
+            }
+        })
+        .catch(err => console.warn(err))
+
     message.channel.send(embed).catch(err => console.error(err))
 };
 
