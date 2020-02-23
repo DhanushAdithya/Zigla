953712274773386f97e4bee656747d097d7434d4
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-02-23 17:59:21 +0530

    Changes in the api

--- commands/meow.js
+++ commands/meow.js
@@ -8,7 +8,7 @@ exports.run = (client, message, args) => {
 
     axios.get('https://api.thecatapi.com/v1/images/search?format=json')
         .then(res => res.data)
-        .then(data => embed.setColor(0x00AE86).setImage(data.url))
+        .then(data => embed.setColor(0x00AE86).setImage(data[0].url))
         .catch(err => console.warn(err))
 
     message.channel.send(embed).catch(err => console.error(err))
