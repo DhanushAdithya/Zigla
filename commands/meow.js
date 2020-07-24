275e27079e8775bdc6709e0061e9478b73c9f727
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-07-24 14:32:47 +0000

    Updated meow.js

--- commands/meow.js
+++ commands/meow.js
@@ -1,19 +1,11 @@
 const {
-    RichEmbed
-} = require('discord.js')
-const embed = new RichEmbed()
-const axios = require('axios');
+    fetchURL
+} = require('../assets')
 
-exports.run = (client, message, args) => {
-
-    axios.get('https://api.thecatapi.com/v1/images/search?format=json')
-        .then(res => res.data)
-        .then(data => embed.setColor(0x00AE86).setImage(data[0].url))
-        .catch(err => console.warn(err))
-
-    message.channel.send(embed).catch(err => console.error(err))
-};
-
-exports.help = {
-    name: 'meow'
+module.exports = {
+    name: 'meow',
+    description: 'Gives you a kitty image',
+    execute(message, args) {
+        fetchURL('https://api.thecatapi.com/v1/images/search?format=json', 'json').then(data => message.channel.send(data[0].url))
+    }
 }
