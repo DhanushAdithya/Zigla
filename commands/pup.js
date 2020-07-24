ca22c6098d31664816b88fe05c4a6db014db3bc8
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-07-24 14:35:49 +0000

    Updated pup.js

--- commands/pup.js
+++ commands/pup.js
@@ -1,35 +1,11 @@
 const {
-    RichEmbed
-} = require('discord.js')
-const embed = new RichEmbed()
-const axios = require('axios');
+    fetchURL
+} = require('../assets')
 
-exports.run = (client, message, args) => {
-
-    axios.get('https://dog.ceo/api/breeds/image/random')
-        .then(res => res.data)
-        .then(data => {
-            l = (data.message).split('/')
-            l = l[4]
-            if (l.includes('-')) {
-                l = l.split('-').map(element => element.charAt(0).toUpperCase() + element.slice(1)).join(' ')
-                embed
-                    .setColor(0x00AE86)
-                    .setImage(data.message)
-                    .setDescription(`**Breed**: ${l}`)
-            } else {
-                l = l.charAt(0).toUpperCase() + l.slice(1)
-                embed
-                    .setColor(0x00AE86)
-                    .setImage(data.message)
-                    .setDescription(`**Breed**: ${l}`)
-            }
-        })
-        .catch(err => console.warn(err))
-
-    message.channel.send(embed).catch(err => console.error(err))
-};
-
-exports.help = {
-    name: 'pup'
+module.exports = {
+    name: 'pup',
+    description: 'Gives you a puppy image',
+    execute(message, args) {
+        fetchURL('https://dog.ceo/api/breeds/image/random', 'json').then(data => message.channel.send(data.message))
+    }
 }
