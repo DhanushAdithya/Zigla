5ad50dbdbcc6d214bbf5c36568ab1aa47c874f82
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-07-26 14:57:20 +0000

    Installed and Initiated Firebase for this bot

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,20 @@
 const fetch = require('node-fetch')
+const firebase = require('firebase')
 const {
     MessageEmbed
 } = require('discord.js')
+
+const firebaseConfig = {
+  apiKey: process.env.FB_KEY,
+  authDomain: `${process.env.FB_ID}.firebaseapp.com`,
+  databaseURL: `https://${process.env.FB_ID}.firebaseio.com`,
+  projectId: process.env.FB_ID,
+  storageBucket: `${process.env.FB_ID}.appspot.com`,
+  messagingSenderId: process.env.MESSAGE_ID,
+  appId: process.env.FB_APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
+};
+firebase.initializeApp(firebaseConfig)
 
 module.exports = {
     fetchURL: async (url, mode) => {
