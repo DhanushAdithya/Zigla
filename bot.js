eadc1fd4c81d1d2f05bfe8cbd76fdc797afc30c5
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-07-28 13:42:55 +0000

    The profanity sanitizer is working fine but it was not nice. So I'll work on this later. For now, no need of it :P

--- bot.js
+++ bot.js
@@ -1,15 +1,15 @@
 const fs = require('fs')
 const { Client, Collection } = require('discord.js')
-const filterFactory = require('profanity-light')
+// const filterFactory = require('profanity-light')
 const { fetchURL } = require('./assets')
 
 const client = new Client()
 client.commands = new Collection()
-const filter = filterFactory.ProfanityFactory({ replacer: _ => _.split('').reverse().join('') })
+// const filter = filterFactory.ProfanityFactory({ replacer: _ => _.split('').reverse().join('') })
 
 /* Thanks to 2Toad/Profanity devs for the profanity words */
 
-fetchURL('https://cdn.jsdelivr.net/gh/2Toad/Profanity/src/words.txt', 'text').then(data => filter.addWords(data.split('\n')))
+// fetchURL('https://cdn.jsdelivr.net/gh/2Toad/Profanity/src/words.txt', 'text').then(data => filter.addWords(data.split('\n')))
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
 
@@ -23,10 +23,10 @@ client.once('ready', () => {
 })
 
 client.on('message', message => {
-    if (!message.author.bot && filter.check(message.content)) {
-        message.channel.send(filter.sanitize(message.content))
-        message.delete()
-    }
+    // if (!message.author.bot && filter.check(message.content)) {
+    //     message.channel.send(filter.sanitize(message.content))
+    //     message.delete()
+    // }
 
     if (!message.content.startsWith(process.env.PREFIX) || message.author.bot) return
 
